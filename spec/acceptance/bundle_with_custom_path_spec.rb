120431b3990bab944900c83fbc448b5a046fc2c0
akihiro17 <coolwizard11@gmail.com>
2015-05-26 13:18:39 +0900

    Run ``` bundle check ``` with ``` BUNDLE_DISABLE_SHARED_GEMS ```  set to '1' if ``` BUNDLE_PATH ``` is specified
    Appraisal does not install gems in the Appraisals file to the ``` BUNDLE_PATH ``` directory if the gems are already installed to another directory.
    
    This commit sets ``` BUNDLE_DISABLE_SHARED_GEMS ``` to '1' and run ``` bundle check ``` if  ``` BUNDLE_PATH ``` is defined in .bundle/config so that Bunlder actually installs gems to the appropriate directory.
    
    I'm not sure that my approach is correct, but in my environment, it looks like working well.
    
    =======
    rbenv 0.4.0-89-g14bc162
    ruby 2.1.0
    bundler 1.9.4
    =======

--- spec/acceptance/bundle_with_custom_path_spec.rb
+++ spec/acceptance/bundle_with_custom_path_spec.rb
@@ -19,4 +19,43 @@ describe "Bundle with custom path" do
     expect(file "gemfiles/breakfast.gemfile").to be_exists
     expect(output).to include("Successfully installed bundler")
   end
+
+  let(:gem_name) { 'activemodel' }
+  let(:path) { 'vendor/bundle' }
+
+  it 'installs gems in the --path directory' do
+    build_gemfile <<-Gemfile
+      source "https://rubygems.org"
+
+      gem '#{gem_name}'
+    Gemfile
+
+    run 'bundle install --path vendor/another-path'
+
+    build_gemfile <<-Gemfile
+      source "https://rubygems.org"
+
+      gem 'appraisal', :path => #{PROJECT_ROOT.inspect}
+    Gemfile
+
+    build_appraisal_file <<-Appraisals
+      appraise "#{gem_name}" do
+        gem '#{gem_name}'
+      end
+    Appraisals
+
+    run %(bundle install --path="#{path}")
+    run 'bundle exec appraisal install'
+
+    result = Dir.glob("tmp/stage/#{path}/ruby/#{RUBY_VERSION}/gems/*")
+             .map    { |path| path.split('/').last }
+             .select { |gem| gem.include?(gem_name) }
+    expect(result).not_to be_empty
+
+    bundle_output = run 'bundle check'
+    expect(bundle_output).to include("The Gemfile's dependencies are satisfied")
+
+    appraisal_output = run 'bundle exec appraisal install'
+    expect(appraisal_output).to include("The Gemfile's dependencies are satisfied")
+  end
 end
